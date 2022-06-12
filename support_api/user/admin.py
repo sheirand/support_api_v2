a6793e703ebfcf6c0576ce2a98576b19e14ac221
evos <eugene.osakovich@gmail.com>
2022-06-12 10:37:04 +0300

    added custom user creation form and custom user admin to be able to create users via admin pannel (with hashed password)

--- support_api/user/admin.py
+++ support_api/user/admin.py
@@ -1,17 +1,41 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
+from django.forms import ModelForm
 
 from user import models
 
 
-class UserAdmin(admin.ModelAdmin):
-    list_display = (
-        "id",
-        "first_name",
-        "last_name",
-        "email"
-    )
-    search_fields = ('email',)
-    list_display_links = ('email',)
+class UserCreationForm(ModelForm):
+    class Meta:
+        model = models.User
+        fields = ('email',)
+
+    def save(self, commit=True):
+        # Save the provided password in hashed format
+        user = super(UserCreationForm, self).save(commit=False)
+        user.set_password(self.cleaned_data["password"])
+        if commit:
+            user.save()
+        return user
 
 
-admin.site.register(models.User, UserAdmin)
+class CustomUserAdmin(UserAdmin):
+    # The forms to add and change user instances
+    add_form = UserCreationForm
+    list_display = ("email",)
+    ordering = ("email",)
+
+    fieldsets = (
+        (None, {'fields': ('email', 'password', 'first_name', 'last_name')}),
+        )
+    add_fieldsets = (
+        (None, {
+            'classes': ('wide',),
+            'fields': ('email', 'password', 'first_name', 'last_name', 'is_superuser', 'is_staff', 'is_active')}
+            ),
+        )
+
+    filter_horizontal = ()
+
+
+admin.site.register(models.User, CustomUserAdmin)

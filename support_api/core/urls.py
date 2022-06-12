f0a602103dc92538c3f68da1ab3d7a3ac11957ac
evos <eugene.osakovich@gmail.com>
2022-06-12 16:34:09 +0300

    finalizing

--- support_api/core/urls.py
+++ support_api/core/urls.py
@@ -1,9 +1,10 @@
 from django.contrib import admin
 from django.urls import include, path
+from drf_yasg import openapi
+from drf_yasg.views import get_schema_view
 from rest_framework import permissions
 from rest_framework.routers import DefaultRouter
-from drf_yasg.views import get_schema_view
-from drf_yasg import openapi
+
 from issue.views import CommentsViewSet, IssueViewSet
 from user.views import (LoginAPIView, LogoutAPIView, RegisterAPIView,
                         UserAPIView)
@@ -16,15 +17,15 @@ router_comment = DefaultRouter()
 router_comment.register('comment', CommentsViewSet, basename="Comment")
 
 schema_view = get_schema_view(
-   openapi.Info(
-      title="Support API",
-      default_version='v1',
-      description="API for Tech Support Sevice",
-      contact=openapi.Contact(email="eugene.osakovich@gmail.com"),
-      license=openapi.License(name="Test License"),
-   ),
-   public=True,
-   permission_classes=(permissions.AllowAny,),
+    openapi.Info(
+        title="Support API",
+        default_version='v1',
+        description="API for Tech Support Sevice",
+        contact=openapi.Contact(email="eugene.osakovich@gmail.com"),
+        license=openapi.License(name="Test License"),
+    ),
+    public=True,
+    permission_classes=(permissions.AllowAny,),
 )
 
 
@@ -36,4 +37,4 @@ urlpatterns = [
     path('api/v1/', include(router_issue.urls)),
     path('api/v1/issue/<int:issue_id>/', include(router_comment.urls)),
     path('schema/', schema_view.with_ui('swagger', cache_timeout=0), name='schema-support-api'),
-    ]
+]
